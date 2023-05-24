c0adb729ea0d3f3e3bab3971926ddd8b657481d3
Sinan Karakaya <sinan.karakaya@epitech.eu>
2023-05-24 22:29:00 +0200

    feat(GUI/all): added build system + dependencies, WindowManager, basic App

---
+++ zappy_gui/src/WindowManager/WindowManager.hpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2023
+** project (Workspace) [WSL: Ubuntu-22.04]
+** File description:
+** WindowManager
+*/
+
+#pragma once
+
+#include <string>
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <spdlog/spdlog.h>
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+#include "WindowManagerException.hpp"
+
+namespace zp {
+
+class WindowManager
+{
+public:
+    WindowManager(const std::string &title, const sf::Vector2u &size);
+    ~WindowManager();
+
+    void update();
+    bool isOpen();
+
+private:
+    void setStyle();
+    void drawImGui();
+    void handleEvents();
+    void render();
+
+    void drawChat();
+    void drawGame();
+    void drawControlPanel();
+
+private:
+    sf::RenderWindow m_window;
+    sf::Clock m_deltaClock;
+
+    sf::RenderTexture m_gameTexture;
+    sf::Sprite m_gameSprite;
+};
+
+} // namespace zp
