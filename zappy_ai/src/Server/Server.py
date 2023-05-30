124342f6b1912391bacabebe73ab7d32131c9eef
RoheeAxel <axel.rohee@epitech.eu>
2023-05-30 11:17:16 +0200

    Feat (Init): add server class

---
+++ zappy_ai/src/Server/Server.py
@@ -0,0 +1,18 @@
+##
+## EPITECH PROJECT, 2023
+## zappy_ai
+## File description:
+## Server
+##
+
+import socket
+
+class Server():
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.socket = None
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.ip, self.port))
