f8b730a4cd4061bb35dae0e99df56c80257780b1
JasonLAGOUTE <jason.lagoute@epitech.eu>
2023-06-06 10:38:05 +0200

    feat(sgt) : add command sgt for graphic protocol

--- zappy_server/src/commands/client_graphic/sgt.c
+++ zappy_server/src/commands/client_graphic/sgt.c
@@ -10,8 +10,13 @@
 
 int sgt(my_zappy_t *zappy, int fd, char **args)
 {
-    (void)zappy;
-    (void)args;
-    (void)fd;
+    char *result = NULL;
+
+    if (zappy == NULL || args == NULL)
+        return 84;
+    if (count_args(args) != 1)
+        return 84;
+    asprintf(&result, "sgt %d\n", zappy->frequency);
+    send_message(fd, result);
     return 0;
 }
