41958d69b19aaaaac60319c17070bd4b1fbd5287
JasonLAGOUTE <jason.lagoute@epitech.eu>
2023-06-10 18:39:42 +0200

    fix(tna) : fix the print of the tna function

--- zappy_server/src/commands/client_graphic/client/tna.c
+++ zappy_server/src/commands/client_graphic/client/tna.c
@@ -12,19 +12,18 @@ int tna(my_zappy_t *zappy, NUSED int fd, cmd_t *cmd)
 {
     char *result = NULL;
     team_t *tmp = NULL;
+
     if (zappy == NULL || cmd->args == NULL)
         return 0;
-    if (count_args(cmd->args) != 1) {
-        add_cmd(cmd, "ko\n");
-        return 0;
-    } tmp = zappy->team_list->first;
+    if (count_args(cmd->args) != 1)
+        return add_cmd(cmd, "ko\n");
+    tmp = zappy->team_list->first;
     result = calloc(1, sizeof(char));
     while (tmp != NULL) {
-        result = realloc(result, sizeof(char) * (strlen(result) + \
-            strlen(tmp->info->name) + 6));
-        result = strcat(result, "tna ");
-        result = strcat(result, tmp->info->name);
-        result = strcat(result, "\n");
+        if (tmp->info->team_id == TEAM_ID_GRAPHIC) {
+            tmp = tmp->next;
+            continue;
+        } asprintf(&result, "%stna %s\n", result, tmp->info->name);
         tmp = tmp->next;
     } add_cmd(cmd, result);
     destroy_struct_team(tmp);
