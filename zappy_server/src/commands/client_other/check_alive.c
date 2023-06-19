1cd98b74fd6e8904827def8b783b7ed6a338eb0d
thomas <thomas.juin@epitech.eu>
2023-06-19 04:58:26 -0400

    feat(check_alive.c) : adding remove id in list

--- zappy_server/src/commands/client_other/check_alive.c
+++ zappy_server/src/commands/client_other/check_alive.c
@@ -23,12 +23,16 @@ static int check_players_in_team(my_zappy_t *zappy, client_t *client)
 int check_alive(my_zappy_t *zappy, int fd)
 {
     client_t *client = get_client_by_fd(zappy->client_list, fd);
+    team_t *team = NULL;
 
     if (client->info->team_id > TEAM_ID_GRAPHIC)
         return 0;
     if (client->info->player->is_alive == false) {
+        team = get_team_by_id(zappy->team_list, client->info->team_id);
         pdi(zappy, fd);
         send_message(fd, DEATH);
+        if (team)
+            remove_id_in_list(team->info->client_list, fd);
         check_players_in_team(zappy, client);
         destroy_client_in_list(zappy->client_list, fd);
         return 1;
