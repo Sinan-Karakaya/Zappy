caa9b6460c70e4615f9d6f73fcfe6e8daf46c06e
thomas <thomas.juin@epitech.eu>
2023-06-20 09:33:43 -0400

    fix(free_parsing.c) : removing printf

--- zappy_server/src/free/free_parsing.c
+++ zappy_server/src/free/free_parsing.c
@@ -15,9 +15,7 @@ size_t free_parsing(parsing_t *parsing)
         return 84;
     if (parsing->names) {
         for (size_t i = 0; parsing->names[i]; i++) {
-            printf("freeing %s\n", parsing->names[i]);
             free(parsing->names[i]);
-            printf("freed\n");
         }
         free(parsing->names);
     }
